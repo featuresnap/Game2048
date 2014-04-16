aa0e86f2c0cd0871e936ab34ebabfd31afe61bc8
Patrick Kelly <pkelly14159@hotmail.com>
2014-04-15 23:12:58 -0400

    Added shiftLeft and padRightZeros

---
+++ Game/GameFunctions.fs
@@ -0,0 +1,29 @@
+﻿module GameFunctions
+
+let rec shiftLeft values = 
+    match values with
+    | 0 :: rest -> shiftLeft rest
+    | x :: y :: rest -> 
+        if x = y then (x + y) :: (shiftLeft rest)
+        else x :: (shiftLeft (y :: rest))
+    | _ -> values
+
+let padRightZeros targetLength list = 
+    let rec zeros = 
+        seq { 
+            yield 0
+            yield! zeros
+        }
+    Seq.concat [ list; zeros ]
+    |> Seq.take targetLength
+    |> List.ofSeq
+
+let swipeLeft values = 
+    values
+    |> shiftLeft
+    |> padRightZeros 4
+
+let swipeRight = 
+    List.rev
+    >> swipeLeft
+    >> List.rev
--- Tests/GameTests.fs
+++ Tests/GameTests.fs
@@ -2,7 +2,52 @@
 
 open FsUnit
 open Xunit
+open GameFunctions
 
 [<Fact>]
-let ``TestTrue`` () = 
-    true |> should equal true
+let ``shiftLeft removes leading zeros``() = 
+    [ 0; 2 ]
+    |> shiftLeft
+    |> should equal [ 2 ]
+
+[<Fact>]
+let ``shiftLeft combines two of same value``() = 
+    [ 0; 2; 2 ]
+    |> shiftLeft
+    |> should equal [ 4 ]
+
+[<Fact>]
+let ``shiftLeft does not combine two different values``() = 
+    [ 0; 2; 4 ]
+    |> shiftLeft
+    |> should equal [ 2; 4 ]
+
+[<Fact>]
+let ``shiftLeft should only combine one level``() = 
+    [ 2; 2; 2; 2 ]
+    |> shiftLeft
+    |> should equal [ 4; 4 ]
+
+[<Fact>]
+let ``shiftLeft prefers combining to left``() = 
+    [ 2; 2; 2; 4 ]
+    |> shiftLeft
+    |> should equal [ 4; 2; 4 ]
+
+[<Fact>]
+let ``shiftLeft prefers combining to left again``() = 
+    [ 4; 2; 2; 2 ]
+    |> shiftLeft
+    |> should equal [ 4; 4; 2 ]
+
+[<Fact>]
+let ``padRightZeros adds zeros to make length the specified value``() = 
+    []
+    |> padRightZeros 4
+    |> should equal [ 0; 0; 0; 0 ]
+
+[<Fact>]
+let ``padRightZeros does not exceed specified length``() = 
+    [ 2; 4; 8; 16 ]
+    |> padRightZeros 4
+    |> should equal [ 2; 4; 8; 16 ]
